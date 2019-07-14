960f8f8960cadca9314c1321065def6917d5d6d5
Annie Truex <artruex@gmail.com>
2019-07-14 21:00:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["butts", "titties", "pussy", "ballsack"]
+ ["my neck", "my back", "my pussy", "my crack"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "arrays!"
+  ["butts"] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[2]
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -31,5 +31,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[-1]
+array.last
 end
